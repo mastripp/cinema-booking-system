fb55f28d4bc79682ff42d4b05012b4b44f127346
Lorenzo Mastrippolito <211658150+mastripp@users.noreply.github.com>
2026-02-15 23:41:59 +0100

    implementato mostraPosti() con display formattato
    - intestazione colonne numerate
    - O per posti liberi, X per prenotati
    
    Closes #2

--- src/main/Cinema.java
+++ src/main/Cinema.java
@@ -8,11 +8,9 @@ public class Cinema {
         this.righe = righe;
         this.colonne = colonne;
         this.posti = new boolean[righe][colonne];
-        for (int i = 0; i < righe; i++) {
-            for (int j = 0; j < colonne; j++) {
+        for (int i = 0; i < righe; i++)
+            for (int j = 0; j < colonne; j++)
                 posti[i][j] = false;
-            }
-        }
     }
 
     public int getRighe() { return righe; }
@@ -21,6 +19,27 @@ public class Cinema {
     public boolean isPostoPrenotato(int riga, int colonna) {
         if (!isValidPosition(riga, colonna)) return false;
         return posti[riga][colonna];
+    }
+
+    public void mostraPosti() {
+        System.out.println("\n=== Mappa della Sala Cinema ===");
+        System.out.println("   Legenda: O = Libero | X = Prenotato\n");
+
+        System.out.print("    ");
+        for (int j = 0; j < colonne; j++) System.out.printf(" %2d", (j + 1));
+        System.out.println();
+
+        System.out.print("    ");
+        for (int j = 0; j < colonne; j++) System.out.print("---");
+        System.out.println();
+
+        for (int i = 0; i < righe; i++) {
+            System.out.printf(" %2d |", (i + 1));
+            for (int j = 0; j < colonne; j++)
+                System.out.print(posti[i][j] ? " X " : " O ");
+            System.out.println();
+        }
+        System.out.println();
     }
 
     private boolean isValidPosition(int riga, int colonna) {
